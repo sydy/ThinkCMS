76da1811adbb7ec22962c33067c76af671360a0d
harker <sydy@vip.qq.com>
2013-11-06 11:50:54 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // 定义ThinkPHP框入口文件
+    define('APP_NAME', 'ThinkCMS');   //定义项目名称
+    define('APP_PATH','./ThinkCMS/');  //定义项目路径
+    define('APP_DEBUG',TRUE);  // 开启调试模式
+    require( "../thinkphp/ThinkPHP/ThinkPHP.php");  // 加载框架入口文件
+    //实例化一个网站应用实例
+	
+?>
